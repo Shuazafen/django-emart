36e862b308b21b026badece6249ba7807544873e
shuazafen <olomoshuaomozafen@gmail.com>
2024-09-04 14:10:29 +0100

    f

--- server/server/shop/urls.py
+++ server/server/shop/urls.py
@@ -3,5 +3,7 @@ from .import views
 urlpatterns = [
     path('', views.home, name="home page"),
     path('detail/<str:model_name>/<int:pk>', views.detail, name="detail"),
-    path('order.html', views.order, name='order' )
+    path('order.html', views.order, name='order' ),
+    path('login.html', views.login, name='login'),
+    path('register.html', views.signup, name='signup')
 ]
--- server/server/shop/views.py
+++ server/server/shop/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
 from django.conf.urls.static import static
+from django.contrib import auth
+from django.contrib.auth import authenticate, login
 from .models import *
 
 def home(request):
@@ -17,5 +20,47 @@ def detail(request, model_name,pk):
 def order(request):
     products = Product.objects.all()
     return render(request, 'order.html', {'products':products})
+
+def signup(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            user = User.objects.create_user(username, email, password1)
+            user.save()
+            auth.login(request, user)
+            return redirect('home')
+        return render(request, 'login.html', {'success': 'Account created successfully. Please log in.'})
+    return render(request,'register.html')
+        
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
+def email_req(request):
+    if request.method == 'post':
+        email = request.POST['email']
+        user = User.objects.filter(email=email)
+        if user.exists():
+            return render(request, 'email_req.html', {'success': 'An email has been sent to the provided address.'})
+        else:
+            return render(request, 'email_req.html', {'error': ''})
+
 # Create your views here.
 
